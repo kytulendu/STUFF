286f4f3254529a0b9c9f4cc09d756437aa69591a
Khralkatorrix <garuda2550@gmail.com>
2026-09-20 00:13:25 +0700

    Add SCT Word Processor to CU-Writer (TIS-620) converter.
    Signed-off-by: Khralkatorrix <garuda2550@gmail.com>

---
+++ TCONV/STC2CW.C
@@ -0,0 +1,78 @@
+/*
+* ============================================================================
+* Convert SCT Word Processor to CU-Writer (TIS-620).
+* By Khralkatorrix <https://github.com/kytulendu>.
+*
+* This is free and unencumbered software released into the public domain.
+*
+* Anyone is free to copy, modify, publish, use, compile, sell, or
+* distribute this software, either in source code form or as a compiled
+* binary, for any purpose, commercial or non-commercial, and by any
+* means.
+
+* In jurisdictions that recognize copyright laws, the author or authors
+* of this software dedicate any and all copyright interest in the
+* software to the public domain. We make this dedication for the benefit
+* of the public at large and to the detriment of our heirs and
+* successors. We intend this dedication to be an overt act of
+* relinquishment in perpetuity of all present and future rights to this
+* software under copyright law.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* For more information, please refer to <http://unlicense.org/>
+*
+* ============================================================================
+*/
+
+#include <stdio.h>
+
+#include "stc2cw.h"
+
+#define __EOF 0x1A /* End of file */
+
+int process(unsigned char *ibuff, unsigned char *obuff, unsigned int lenght)
+{
+    unsigned char c;
+    unsigned int i;
+    int count = 0;
+
+    for (i = 0; i < lenght; i++)
+    {
+        c = *ibuff;
+        /* skip option at the end of file */
+        if (c == __EOF)
+        {
+            break;
+        }
+
+        /* control code */
+        if (c == 0x13)              	/* bold? */
+        {
+            c = 0x02;
+        }
+		else if (c == 0x8a)         	/* Soft line feed */
+        {
+            c = 0x0a;
+        }
+
+        *obuff = stc2cw(c);
+        ibuff++;
+        obuff++;
+        count++;
+    }
+    return count;
+}
+
+void usage(void)
+{
+    fprintf(stderr, "Convert SCT Word Processor to CU-Writer (TIS-620).\n");
+    fprintf(stderr, "By Khralkatorrix.\n\n");
+    fprintf(stderr, "Usage: stc2cw <input file> <output file>\n");
+}
